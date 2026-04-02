af6497b7117569b6d37a7ea65e4696834b46d1f3
fabiokounang <fabiokounang11@gmail.com>
2026-04-02 17:22:24 +0700

    finish

--- routes/merchant/orders.js
+++ routes/merchant/orders.js
@@ -2,9 +2,9 @@ const router = require("express").Router();
 const orders = require("../../controllers/merchant/orders");
 const { requireAuth, requireRole } = require("../../middleware/auth");
 
-router.post("/create-qris", orders.createOrderAndGenerateQris);
+router.post("/create-qris", requireAuth, requireRole("merchant"), orders.createOrderAndGenerateQris);
 router.get("/", requireAuth, requireRole("merchant"), orders.list);
-router.get("/temp", (req, res) => {
+router.get("/temp", requireAuth, requireRole("merchant"), (req, res) => {
   return res.render("temp", {
     error: null,
     success: null,
