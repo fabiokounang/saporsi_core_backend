db81d6d6a1e509aa81930c8b98c60ad53b24eada
fabiokounang <fabiokounang11@gmail.com>
2026-04-02 21:53:07 +0700

    finish

--- index.js
+++ index.js
@@ -212,3 +212,5 @@ const PORT = Number(process.env.PORT || 3000);
 app.listen(PORT, () => {
   console.log(`Saporsi Core running on port ${PORT}`);
 });
+
+console.log(process.env.JWT_SECRET, 'JWT SECRET');
